cb38090bc54a2f3b9cf9488a6787e6045653662f
Gleb Klopotov <gleb.klopotov@yandex.ru>
2024-04-02 18:07:57 +0300

    First lesson

--- first_page.py
+++ first_page.py
@@ -1,2 +1,9 @@
-print("Start")
-print('Вторая строка')
+import telebot
+
+bot = telebot.TeleBot('6676623782:AAHaL32hiqxhpfDSu_IV_-8JKcx4-irndCo')
+
+@bot.message_handler(commands=['start'])
+def main(message):  # message ранит в себе чат с пользователем
+    bot.send_message(message.chat.id, 'Привет!') # message.chat.id - это id текущего чата с пользователем
+
+bot.infinity_polling() # позволяет не завершать работу скрипта, т.е. не останавливать бота
