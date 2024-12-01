e6d624d4e78e9c4e7116715c9ea84b1849bd2aae
gleb klopotov <giklopotov@miem.hse.ru>
2024-12-01 21:05:12 +0300

    questions dict

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 note_link = 'https://habr.com/ru/articles/821961/'
 full_note_link = 'https://docs.aiogram.dev/en/stable/api/types/inaccessible_message.html#aiogram.types.inaccessible_message.InaccessibleMessage.answer_photo'
+
+qustions = {
+    'Вопрос 1': 'Ответ 1',
+    'Назад': 'Выберите пункт меню'
+}
--- keyboards.py
+++ keyboards.py
@@ -9,9 +9,10 @@ main = ReplyKeyboardMarkup(keyboard=[
     input_field_placeholder='Выбери пункт меню.')
 
 questions_keyboard = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='О продукте')],
-    [KeyboardButton(text='Часто задаваемые вопросы')],
-    [KeyboardButton(text='Купить тетрадь')]
+    [KeyboardButton(text='Вопрос 1')],
+    [KeyboardButton(text='Вопрос 2')],
+    [KeyboardButton(text='Вопрос 3')],
+    [KeyboardButton(text='Назад')]
 ],
     resize_keyboard=True,
     input_field_placeholder='Выбери пункт меню.')
